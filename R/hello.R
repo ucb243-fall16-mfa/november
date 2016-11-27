578f61a152dc8e43a7c0d626beed7e3e929eca53
Jason Sellers <jason@Jasons-MacBook-Air.local>
2016-11-26 19:16:34 -0800

    allow for varying set dimensions

--- R/hello.R
+++ R/hello.R
@@ -20,22 +20,37 @@ hello <- function() {
 setwd("../Documents/stat243-fall-2016/problem-sets/final-project/")
 df <- read.csv("data/wines.csv", stringsAsFactors = FALSE)
 
-mfa <- function(data, sets=NULL, ncomps = NULL, center = TRUE, scale = TRUE) {
+
+mfa <- function(data, sets=list(2:7,8:13,14:19,20:23,24:30,31:35,36:39,40:45,46:50,51:54),
+                ncomps = NULL, center = TRUE, scale = TRUE) {
+  # call helper function to check that params are in proper format
   check_params(data, sets)
 
-  X <- as.matrix(df[,2:54])
+  # pull out data columns specified in sets
+  X <- sapply(sets, function(x) data[,x][])
+  # merge list of matrices into a single matrix
+  X <- do.call(cbind, X)
+  # specify that data is formatted as a matrix
+  X <- as.matrix(X)
+
+  # adjust set values to reduced matrix dimensions
+  sets.len <- sapply(sets, function(x) length(x))
+  start <- cumsum(sets.len) - sets.len + 1
+  end <- cumsum(sets.len)
+  sets <- sapply(1:length(sets.len), function(i) start[i]:end[i])
+
+  # center and scale values
   X <- apply(X, 2, function(x) x - mean(x))
   X <- apply(X, 2, function(x) x / sqrt(sum(x * x)))
 
-  start <- c(1,7,13,19,23,30,35,39,45,50)
-  end <- c(6,12,18,22,29,34,38,44,49,53)
-  loc <- cbind(start,end)
-  loc <- loc[sets,]
-
-  a <- get_alphas(X,loc)
-  A <- get_A(a,X,loc)
+  # function call to compute the alpha weight vector
+  a <- get_alphas(X,sets)
+  # function call to convert alpha vector into A vector
+  A <- get_A(a, sets.len)
+  # compute M vector for diagonal mass matrix
   M <- diag(rep(1 / nrow(X), nrow(X)))
 
+  # function to compute generalized PCA
   A.tilde <- sqrt(M) %*% X %*% sqrt(A)
   A.tilde.svd <- svd(A.tilde)
   U.tilde <- solve(sqrt(M)) %*% A.tilde.svd$u
@@ -43,6 +58,7 @@ mfa <- function(data, sets=NULL, ncomps = NULL, center = TRUE, scale = TRUE) {
   D.tilde <- A.tilde.svd$d
   loadings <- V.tilde
   factor_scores <- U.tilde %*% diag(D.tilde)
+
 
   P <- array(0, c(12,12,10))
   for (i in 1:nrow(loc)) {
@@ -74,22 +90,23 @@ check_params <-function(data, sets) {
   return(TRUE)
 }
 
-get_alphas <- function(X,loc) {
+# function to compute the alpha weight vector
+get_alphas <- function(X, sets) {
+  # compute alpha value for a submatrix
   alpha_helper <- function(X) {
     X.svd <- svd(X)
     a <- 1 / X.svd$d[1]^2
     return(a)
   }
-  alphas <- apply(loc,1,function(x) alpha_helper(X[,x[1]:x[2]]))
+  # call helper function for each submatrix
+  alphas <- sapply(sets, function(i) alpha_helper(X[,i]))
 }
 
-get_A <- function(a, X, loc) {
-  l <- rep(0,ncol(X))
-  for (i in 1:nrow(loc)) {
-    s <- loc[i,2] - loc[i,1] + 1
-    l[loc[i,1]:loc[i,2]] <- rep(a[i], s)
-  }
-  return(diag(l))
+# function to convert the alpha weight vector to a matrix
+get_A <- function(a, sets.len) {
+  a.rep <- sapply(1:length(a), function(i) rep(a[i], sets.len[i]))
+  a.rep <- unlist(a.rep)
+  return(diag(a.rep))
 }
 
 print.mfa <- function(x, ...) {
@@ -164,16 +181,24 @@ tbl_to_dim <- function(mfa,loc) {
 }
 
 RV <- function(table1, table2) {
+  table1 <- as.matrix(table1)
+  table2 <- as.matrix(table2)
   num <- sum(diag((tcrossprod(table1) %*% tcrossprod(table2))))
   den <- sqrt(sum(diag(tcrossprod(table1) %*% tcrossprod(table1))) *
               sum(diag(tcrossprod(table2) %*% tcrossprod(table2))))
   return(num/den)
 }
 
-RV_table <- function(dataset, sets=list(1:3,4:5,6:10)) {
+
+
+RV_table <- function(df, sets=list(1:3,4:5,6:10)) {
   M <- diag(length(sets))
-  for (i in 1:length(sets)) {
-
+  combos <- combn(1:length(sets),2)
+  for (i in 1:ncol(combos)) {
+    a <- combos[1,i]
+    b <- combos[2,i]
+    M[a,b] <- RV(df[,sets[[a]]],df[,sets[[b]]])
+    M[b,a] <- RV(df[,sets[[a]]],df[,sets[[b]]])
   }
-
+  return(M)
 }
